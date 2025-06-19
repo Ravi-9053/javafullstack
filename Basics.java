2d1e541815037d9db2e47d02f97614caf83cd355
Ravi-9053 <ravikumartillari62@gmail.com>
2025-06-19 15:14:40 +0530

    Create Basics.java

---
+++ Basics.java
@@ -0,0 +1,56 @@
+/*
+public class MyClass //Using class And object 
+{ 
+// Method signature: public String greet(String name)
+public String greet(String name) 
+{ 
+return "WELCOME TO " + name + "!";
+}
+public static void main(String[] args) 
+{
+MyClass obj = new MyClass();
+String message = obj.greet("MY WORLD"); 
+System.out.println(message);
+}
+}
+*/
+
+/*
+public class MyClass // print your name
+{
+public static void main(String[] args) 
+{
+System.out.println("Ravikumar");
+}
+}
+*/
+/* 
+//This is a documentation comment.
+public class MyClass 
+{
+public static void main(String[] args) 
+{
+// This is a single-line comment.
+System.out.println("Believe yourself");
+//This is a multi-line comment.
+System.out.println("Welcome to My World");
+}
+}
+*/
+
+public class MyClass //Data Types
+{
+public static void main(String[] args) 
+{
+int myInt = 10;
+boolean myBoolean = true;
+char myChar = 'A';
+float myFloat = 10.5f;
+double myDouble = 20.75;
+System.out.println("Integer: " + myInt);
+System.out.println("Boolean: " + myBoolean);
+System.out.println("Character: " + myChar);
+System.out.println("Float: " + myFloat);
+System.out.println("Double: " + myDouble);
+}
+}
