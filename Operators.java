5c8d87399137c5a354fe9cda4c26cf35b7f96424
Ravi-9053 <ravikumartillari62@gmail.com>
2025-06-23 18:38:43 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,109 @@
+/*
+public class Operators
+{
+public static void main(String[] args)
+{
+int a=10;
+int b=5;
+System.out.println("Add:"+(a+b));
+System.out.println("Sub:"+(a-b));
+System.out.println("Mul:"+(a*b));
+System.out.println("Div:"+(a/b));
+}
+}
+*/
+
+/*
+public class Operators
+{
+public static void main(String[] args)
+{
+int x=5;
+System.out.println("Pre-Inc:"+(++x));
+System.out.println("Post-Inc:"+(x++));
+System.out.println("Pre-Dec:"+(--x));
+System.out.println("Post-Dec:"+(x--));
+System.out.println("Final:"+x);
+}
+}
+*/
+
+/*
+public class Operators
+{
+public static void main(String[] args)
+{
+int a=10;
+int b=20;
+System.out.println("Equal:"+(a==b));
+System.out.println("Not Equal:"+(a!=b));
+}
+}
+*/
+
+/*
+public class Operators
+{
+public static void main(String[] args)
+{
+int a=15;
+int b=15;
+if(a==b)
+{
+System.out.println("Both Equal");
+}
+else
+{
+System.out.println("Not Equal");
+}
+}
+}
+*/
+
+/*
+public class Operators
+{
+public static void main(String[] args)
+{
+boolean x=true;
+boolean y=false;
+System.out.println("AND:"+(x&&y));
+System.out.println("OR:"+(x||y));
+System.out.println("NOT:"+(!x));
+}
+}
+*/
+
+/*
+public class Operators
+{
+public static void main(String[] args)
+{
+int a=10;
+int b=20;
+System.out.println("<:"+(a<b));
+System.out.println("<=:"+(a<=b));
+System.out.println(">:"+(a>b));
+System.out.println(">=:"+(a>=b));
+}
+}
+*/
+
+public class Operators
+{
+public static void main(String[] args)
+{
+int a=15;
+int b=25;
+if(a>b)
+{
+System.out.println("Larger:"+a);
+System.out.println("Smaller:"+b);
+}
+else
+{
+System.out.println("Larger:"+b);
+System.out.println("Smaller:"+a);
+}
+}
+}
